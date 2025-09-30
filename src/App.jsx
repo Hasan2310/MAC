53b934408b63cbd52184f4f75d227e3984f4227d
Hasan2310 <hasanahmad04273@gmail.com>
2025-09-30 11:09:14 +0700

    'UPDATE'

--- src/App.jsx
+++ src/App.jsx
@@ -53,6 +53,7 @@ const App = () => {
       try {
         const res = await fetch("https://script.google.com/macros/s/AKfycbwCO5Qs2cozq4zuHYUpYgjnf65nD0G0C8HkNN9IXXPD3_CKkk-5HjyQJEn3SogDoyxdmw/exec"); // ← Ganti dengan URL kamu
         const data = await res.json();
+        cors: "no-cors",
         setStock((prev) => ({ ...prev, wood: Number(data.wood) || 0 }));
       } catch (error) {
         console.error("Gagal ambil data stock:", error);
