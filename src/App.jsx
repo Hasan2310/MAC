b7a915745e44939ca321ffc9c07e84dd9316c738
Hasan2310 <hasanahmad04273@gmail.com>
2025-09-30 18:12:46 +0700

    'UPDATE'

--- src/App.jsx
+++ src/App.jsx
@@ -51,7 +51,7 @@ const App = () => {
   useEffect(() => {
     const fetchStock = async () => {
       try {
-        const res = await fetch("/api/fetchStock");
+        const res = await fetch("https://script.google.com/macros/s/AKfycbx9trvEtmIJw6HWJtyMoTJdPnl5SN35z4p3mlZwFE1BzOKwb227p13KY6sEyBoD34jFSw/exec");
         const data = await res.json();
         setStock((prev) => ({ ...prev, wood: Number(data.wood) || 0 }));
       } catch (error) {
@@ -198,7 +198,7 @@ Arrow Carbon:
 
     try {
       setIsLoading(true);
-      const res = await fetch("/api/sendReport", {
+      const res = await fetch("https://script.google.com/macros/s/AKfycbx9trvEtmIJw6HWJtyMoTJdPnl5SN35z4p3mlZwFE1BzOKwb227p13KY6sEyBoD34jFSw/exec", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
