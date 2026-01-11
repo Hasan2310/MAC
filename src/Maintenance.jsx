a4c9b25d6f8c91eafa4ae59bcbb61c415f051340
Hasan2310 <hasanahmad04273@gmail.com>
2026-01-11 12:28:40 +0700

    'UPDATE'

--- src/Maintenance.jsx
+++ src/Maintenance.jsx
@@ -2,27 +2,27 @@ import { useEffect } from "react";
 import Swal from "sweetalert2";
 
 const Maintenance = () => {
-  useEffect(() => {
-    if (sessionStorage.getItem("maintenanceShown")) return;
-    sessionStorage.setItem("maintenanceShown", "true");
+//   useEffect(() => {
+//     if (sessionStorage.getItem("maintenanceShown")) return;
+//     sessionStorage.setItem("maintenanceShown", "true");
 
-    Swal.fire({
-      icon: "info",
-      title: "Maintenance Notice",
-      html: `
-        <p style="margin-top:8px">
-          Saat ini sistem sedang dalam proses pemeliharaan.<br/>
-          Beberapa fitur mungkin tidak tersedia untuk sementara waktu.
-        </p>
-      `,
-      background: "#ffffff",
-      color: "#233975",
-      confirmButtonText: "Mengerti",
-      confirmButtonColor: "#223B7D",
-      allowOutsideClick: false,
-      allowEscapeKey: false,
-    });
-  }, []);
+//     Swal.fire({
+//       icon: "info",
+//       title: "Maintenance Notice",
+//       html: `
+//         <p style="margin-top:8px">
+//           Saat ini sistem sedang dalam proses pemeliharaan.<br/>
+//           Beberapa fitur mungkin tidak tersedia untuk sementara waktu.
+//         </p>
+//       `,
+//       background: "#ffffff",
+//       color: "#233975",
+//       confirmButtonText: "Mengerti",
+//       confirmButtonColor: "#223B7D",
+//       allowOutsideClick: false,
+//       allowEscapeKey: false,
+//     });
+//   }, []);
 
   return (
     <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100 text-center px-6">
